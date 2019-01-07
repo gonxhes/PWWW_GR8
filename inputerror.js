fa0ac945e4c86fa46176a5a00a6578e19b79d96a
Gonxhe <gonxhe.sylaj@gmail.com>
2019-01-07 12:40:34 +0100

    Ndarja e JS nga dokumenti Game.html

---
+++ inputerror.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var message, x;
+  message = document.getElementById("type");
+  message.innerHTML = "";
+  x = document.getElementById("rate").value;
+  try { 
+    if(x == "")  throw "empty";
+    if(isNaN(x)) throw "not a number";
+    x = Number(x);
+    if(x < 1)  throw "too low";
+    if(x > 10)   throw "too high";
+  }
+  catch(err) {
+    message.innerHTML = "Input is " + err;
+  }
+}
